3f03db7b75a1a84f429e573c561233cfdc018d59
chientrm <chientrm@gmail.com>
2022-11-08 23:43:26 +0000

    init

--- todos/config/server.ts
+++ todos/config/server.ts
@@ -1,7 +1,27 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  const port = 1337;
+  if (process.env.GITPOD_WORKSPACE_URL) {
+    const url = process.env.GITPOD_WORKSPACE_URL.replace(
+      "https://",
+      `https://${port}-`
+    );
+    return {
+      host: env("HOST", "0.0.0.0"),
+      url,
+      port: env.int("PORT", port),
+      app: {
+        keys: env.array("APP_KEYS"),
+      },
+      admin: {
+        url: `${url}/admin`,
+      },
+    };
+  }
+  return {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: env.array("APP_KEYS"),
+    },
+  };
+};
